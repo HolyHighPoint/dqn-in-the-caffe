d41e143f32dcc1c41dcba8302cde83c42fd0f69a
jiguanglizipao <jiguanglizipao@gmail.com>
2015-07-04 20:36:31 +0800

    Add comments

--- source/qcli/GameDQNBridge.cpp
+++ source/qcli/GameDQNBridge.cpp
@@ -18,28 +18,33 @@ GameDQNBridge::GameDQNBridge(ALEInterface& ale, qlearning::DQN& dqn, const bool 
 
 double GameDQNBridge::play(double epsilon) {
 	qlearning::ALE ale;
-    std::deque<qlearning::FDataP> past;
+    std::deque<qlearning::FDataP> past;		//frame sequence
     double total = 0.0;
     for (int frame = 0; !ale_.game_over(); frame++) {
         const auto current = ale.PreprocessScreen(ale_.getScreen());
         past.push_back(current);
+        //fill frame sequence
         if (past.size() < qlearning::ICount) {
             for (int i = 0; i < skip_frame + 1 && !ale_.game_over(); i++) {
                 total += ale_.act(PLAYER_A_NOOP);
             }
         } else {
+			//pop history frame
             if (past.size() > qlearning::ICount) {
                 past.pop_front();
             }
             qlearning::IFrames input;
             std::copy(past.begin(), past.end(), input.begin());
+            //get action
             const Action action = dqn_.SelectAction(input, epsilon);
+            //get recent score
             double recent = 0.0;
             for (int i = 0; i < skip_frame + 1 && !ale_.game_over(); i++) {
                 recent += ale_.act(action);
             }
             total += recent;
             const double reward = (recent == 0 ? 0 : recent /= std::abs(recent));
+            //update solver
             if (update_) {
                 const auto trans = ale_.game_over() ? qlearning::Trans(input, action, reward, boost::none) : qlearning::Trans(input, action, reward, ale.PreprocessScreen(ale_.getScreen()));
                 dqn_.AddTrans(trans);
